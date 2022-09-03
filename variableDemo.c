f7b0ddfa722e2fe30c8b6ba59daf024ad47949e4
Jaffar008 <93038384+Jaffar008@users.noreply.github.com>
2022-09-03 19:33:28 +0530

    Add files via upload

---
+++ variableDemo.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a;
+    float b;
+
+    printf("Enter a integer value and a float value:");
+    scanf("%d%f",&a,&b);
+    printf("The value of a is %d and value of b is %f.",a,b);
+}
